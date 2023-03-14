53f860d04ede04166215b9a38addca60e810a415
JJ <javier_5_2010@hotmail.com>
2023-03-14 13:51:59 +0100

    XD

--- src/Ejemplo.java
+++ src/Ejemplo.java
@@ -2,5 +2,7 @@ public class Ejemplo {
     public static void main(){
         System.out.println("HOLA MUNDO");
         String cadena = "XD";
+        System.out.println(cadena);
+        System.out.print("XD O xd");
     }
 }
